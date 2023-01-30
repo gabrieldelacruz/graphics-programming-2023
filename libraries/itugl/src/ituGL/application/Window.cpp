c839462260e69ba15a032c667ff51f188a8ad386
Gabriel de la Cruz <syxtem@yahoo.com>
2023-01-30 21:48:54 +0100

    exercise00.0

--- libraries/itugl/src/ituGL/application/Window.cpp
+++ libraries/itugl/src/ituGL/application/Window.cpp
@@ -3,30 +3,44 @@
 // Create the internal GLFW window. We provide some hints about it to OpenGL
 Window::Window(int width, int height, const char* title) : m_window(nullptr)
 {
+    // Set some hints for window creation
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    m_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 }
 
 // If we have an internal GLFW window, destroy it
 Window::~Window()
 {
+    if (m_window)
+    {
+        glfwDestroyWindow(m_window);
+    }
 }
 
 // Get the current dimensions (width and height) of the window
 void Window::GetDimensions(int& width, int& height) const
 {
+    glfwGetWindowSize(m_window, &width, &height);
 }
 
 // Tell the window that it should close
 void Window::Close()
 {
+    glfwSetWindowShouldClose(m_window, GL_TRUE);
 }
 
 // Get if the window should be closed this frame
 bool Window::ShouldClose() const
 {
-    return false;
+    return glfwWindowShouldClose(m_window);
 }
 
 // Swaps the front and back buffers of the window
 void Window::SwapBuffers()
 {
+    glfwSwapBuffers(m_window);
 }
--- libraries/itugl/src/ituGL/core/DeviceGL.cpp
+++ libraries/itugl/src/ituGL/core/DeviceGL.cpp
@@ -8,33 +8,61 @@ DeviceGL* DeviceGL::m_instance = nullptr;
 
 DeviceGL::DeviceGL() : m_contextLoaded(false)
 {
+    m_instance = this;
+
+    // Init GLFW
+    glfwInit();
 }
 
 DeviceGL::~DeviceGL()
 {
+    m_instance = nullptr;
+
+    // Terminate GLFW
+    glfwTerminate();
 }
 
 // Set the window that OpenGL will use for rendering
 void DeviceGL::SetCurrentWindow(Window& window)
 {
+    GLFWwindow* glfwWindow = window.GetInternalWindow();
+    glfwMakeContextCurrent(glfwWindow);
+
+    // Load required GL libraries and initialize the context
+    m_contextLoaded = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    if (m_contextLoaded)
+    {
+        // Set callback to be called when the window is resized
+        glfwSetFramebufferSizeCallback(glfwWindow, FrameBufferResized);
+    }
 }
 
 // Set the dimensions of the viewport
 void DeviceGL::SetViewport(GLint x, GLint y, GLsizei width, GLsizei height)
 {
+    glViewport(0, 0, width, height);
 }
 
 // Poll the events in the window event queue
 void DeviceGL::PollEvents()
 {
+    glfwPollEvents();
 }
 
 // Clear the framebuffer with the specified color
 void DeviceGL::Clear(float r, float g, float b, float a)
 {
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 // Callback called when the framebuffer changes size
 void DeviceGL::FrameBufferResized(GLFWwindow* window, GLsizei width, GLsizei height)
 {
+    if (m_instance)
+    {
+        // Adjust the viewport when the framebuffer is resized
+        m_instance->SetViewport(0, 0, width, height);
+    }
 }
