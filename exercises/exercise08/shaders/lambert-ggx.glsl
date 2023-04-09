a0c09a340e11179e1a46ea9dbdb03c0f443e368e
Gabriel de la Cruz <syxtem@yahoo.com>
2023-04-10 00:04:33 +0200

    exercise08.5

--- exercises/exercise08/shaders/lambert-ggx.glsl
+++ exercises/exercise08/shaders/lambert-ggx.glsl
@@ -45,8 +45,13 @@ vec3 FresnelSchlick(vec3 f0, vec3 viewDir, vec3 halfDir)
 // GGX equation for distribution function
 float DistributionGGX(vec3 normal, vec3 halfDir, float roughness)
 {
-	// (todo) 08.5: Implement the equation
-	return vec3(0.0f);
+	float roughness2 = roughness * roughness;
+
+	float dotNH = ClampedDot(normal, halfDir);
+
+	float expr = dotNH * dotNH * (roughness2 - 1.0) + 1.0;
+
+	return roughness2 / (Pi * expr * expr);
 }
 
 // Geometry term in one direction, for GGX equation
@@ -118,8 +123,16 @@ vec3 ComputeDiffuseLighting(SurfaceData data, vec3 lightDir)
 
 vec3 ComputeSpecularLighting(SurfaceData data, vec3 lightDir, vec3 viewDir)
 {
-	// (todo) 08.5: Implement the Cook-Torrance equation using the D (distribution) and G (geometry) terms
-	return vec3(0.0f);
+	// Implement the Cook-Torrance equation using the D (distribution) and G (geometry) terms
+	vec3 halfDir = normalize(lightDir + viewDir);
+
+	float D = DistributionGGX(data.normal, halfDir, data.roughness);
+	float G = GeometrySmith(data.normal, lightDir, viewDir, data.roughness);
+
+	float cosI = ClampedDot(data.normal, lightDir);
+	float cosO = ClampedDot(data.normal, viewDir);
+
+	return vec3((D * G) / (4.0f * cosO * cosI + 0.00001f));
 }
 
 vec3 CombineLighting(vec3 diffuse, vec3 specular, SurfaceData data, vec3 lightDir, vec3 viewDir)
